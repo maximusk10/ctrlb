2e907d691f48d04ac8f6051bd6ee97c12ed88841
cristobal ponce <hawkfx@gmail.com>
2018-03-02 14:55:13 -0600

    Initial set of Files

---
+++ app/Providers/SparkServiceProvider.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Providers;
+
+use Laravel\Spark\Spark;
+use Laravel\Spark\Providers\AppServiceProvider as ServiceProvider;
+
+class SparkServiceProvider extends ServiceProvider
+{
+    /**
+     * Your application and company details.
+     *
+     * @var array
+     */
+    protected $details = [
+        'vendor' => 'Clariad',
+        'product' => 'CtrlBook',
+        'street' => 'PO Box 111',
+        'location' => 'Your Town, NY 12345',
+        'phone' => '555-555-5555',
+    ];
+
+    /**
+     * The address where customer support e-mails should be sent.
+     *
+     * @var string
+     */
+    protected $sendSupportEmailsTo = null;
+
+    /**
+     * All of the application developer e-mail addresses.
+     *
+     * @var array
+     */
+    protected $developers = [
+        //
+        'hawkfx@gmail.com'
+    ];
+
+    /**
+     * Indicates if the application will expose an API.
+     *
+     * @var bool
+     */
+    protected $usesApi = true;
+
+    /**
+     * Finish configuring Spark for the application.
+     *
+     * @return void
+     */
+    public function booted()
+    {
+        Spark::useStripe()->noCardUpFront()->teamTrialDays(10);
+
+        Spark::freeTeamPlan()
+            ->features([
+                '1 FB Account',
+                '1 User'
+            ]);
+
+        Spark::teamPlan('Basic', 'provider-id-1')
+            ->price(10)
+            ->features([
+                '100 FB Accounts',
+                '10 Users'
+            ]);
+    }
+}
